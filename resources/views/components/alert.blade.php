477daa3996397a9139c8acac9fa24f7445e3e015
hafiziiqbal <hafiziiqbal12345@gmail.com>
2023-02-28 19:57:07 +0700

    add alert component

---
+++ resources/views/components/alert.blade.php
@@ -0,0 +1,15 @@
+@if ($errors->has('error'))
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <i class="fa fa-warning"></i>
+        {{ $errors->first('error') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+@endif
+
+@if (session()->has('success'))
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        <i class="fa fa-check"></i>
+        {{ session()->get('success') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+@endif
