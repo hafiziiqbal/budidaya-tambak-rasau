5592df7d33b27976d40bbef809c4541835898210
hafiziiqbal <hafiziiqbal12345@gmail.com>
2023-02-28 19:56:55 +0700

    add login page

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.default')
+@section('content')
+    <div class="bg-dark">
+        <div id="layoutAuthentication">
+            <div id="layoutAuthentication_content">
+                <main>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-5">
+                                <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                    @include('components.alert')
+                                    <div class="card-header">
+                                        <h3 class="text-center font-weight-light my-4">Login</h3>
+                                    </div>
+                                    <div class="card-body">
+                                        <form action="{{ route('login') }}" method="POST">
+                                            @csrf
+                                            <div class="form-floating mb-3">
+                                                <input class="form-control" id="inputEmail" type="email" name="email"
+                                                    value="{{ old('email') }}" placeholder="name@example.com" />
+                                                <label for="inputEmail">Email</label>
+                                            </div>
+                                            <div class="form-floating mb-3">
+                                                <input class="form-control" id="inputPassword" type="password"
+                                                    value="{{ old('password') }}" name="password" placeholder="Password"
+                                                    required />
+                                                <label for="inputPassword">Password</label>
+                                            </div>
+                                            <div class="form-check mb-3">
+                                                <input class="form-check-input" id="inputRememberPassword" type="checkbox"
+                                                    name="remember" />
+                                                <label class="form-check-label" for="inputRememberPassword">Remember
+                                                    Password</label>
+                                            </div>
+                                            <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+                                                <a class="small" href="password.html">Lupa Password?</a>
+                                                <button type="submit" class="btn btn-primary">Masuk</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+            <div id="layoutAuthentication_footer">
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-center small">
+                            <div class="text-muted">Copyright &copy; {{ env('APP_NAME') }} 2022</div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+    </div>
+@endsection
