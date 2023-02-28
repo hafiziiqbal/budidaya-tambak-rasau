3bc94b8c779635c77c78f80b7147dbc5737bdc21
hafiziiqbal <hafiziiqbal12345@gmail.com>
2023-02-28 19:56:47 +0700

    add home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.default')
+@section('content')
+    <div class="container">
+        <div class="d-flex align-items-center justify-content-center" style="height:75vh">
+            <a href="{{ route('login') }}" class="btn btn-primary">Masuk</a>
+        </div>
+    </div>
+    
+@endsection
