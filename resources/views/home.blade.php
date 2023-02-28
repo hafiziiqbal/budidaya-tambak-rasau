b5935cf0e6eb5298188da8c0fae45df776f5d95d
hafiziiqbal <hafiziiqbal12345@gmail.com>
2023-02-28 21:07:45 +0700

    add alert in home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,13 @@
 @extends('layouts.default')
 @section('content')
     <div class="container">
+
         <div class="d-flex align-items-center justify-content-center" style="height:75vh">
-            <a href="{{ route('login') }}" class="btn btn-primary">Masuk</a>
+            <div>
+                @include('components.alert')
+                <a href="{{ route('login') }}" class="btn btn-primary my-3">Masuk</a>
+            </div>
+
         </div>
     </div>
-    
 @endsection
