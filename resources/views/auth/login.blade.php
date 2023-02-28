a5a048974a4a6949a3c8755eee710eea4d30180f
hafiziiqbal <hafiziiqbal12345@gmail.com>
2023-02-28 20:15:57 +0700

    change footer login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -48,7 +48,7 @@
                 <footer class="py-4 bg-light mt-auto">
                     <div class="container-fluid px-4">
                         <div class="d-flex align-items-center justify-content-center small">
-                            <div class="text-muted">Copyright &copy; {{ env('APP_NAME') }} 2022</div>
+                            <div class="text-muted">Copyright &copy; {{ env('APP_NAME') }} 2023</div>
                         </div>
                     </div>
                 </footer>
