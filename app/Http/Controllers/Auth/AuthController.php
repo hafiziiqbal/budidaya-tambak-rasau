16803157e8ad062d16002ec08aca7ab7c9cebb21
hafiziiqbal <hafiziiqbal12345@gmail.com>
2023-02-28 20:23:50 +0700

    add logout function

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -18,12 +18,17 @@ class AuthController extends Controller
     public function authenticate(Request $request)
     {
         if (Auth::attempt(['email' => $request->email, 'password' => $request->password], $request->remember)) {
-            // return redirect()->route('');
-            return 'masuk';
+            return redirect()->route('dashboard');
         } else {
             return back()->withErrors([
                 'error' => 'Akun yang Anda Masukan Tidak Terdeteksi'
             ])->withInput();
         }
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        return redirect('/');
     }
 }
